a287731495cf671f2f654899dbe52ecdfe54a6ca
Leela0123 <117524740+Leela0123@users.noreply.github.com>
2023-02-12 14:00:19 +0530

    Update PhotoFrame.js

--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export const PhotoFrame = () => {
-   
+   return(<div classNane ="photoframe">
+          <img src ="url"/>
+      </div>
+          <div className ="caption>
+          <title/>
+          </div>
+         )
 }
